be003fd2c184743a9e899b98afbcff6f8a4cc599
Levente Hunyadi <hunyadi@gmail.com>
2022-11-14 18:46:54 +0100

    Improve Base64 encoding performance

--- include/persistence/base64.hpp
+++ include/persistence/base64.hpp
@@ -7,6 +7,19 @@ namespace persistence
 {
     namespace detail
     {
+        enum class endianness
+        {
+        #ifdef _WIN32
+            little = 0,
+            big    = 1,
+            native = little
+        #else
+            little = __ORDER_LITTLE_ENDIAN__,
+            big    = __ORDER_BIG_ENDIAN__,
+            native = __BYTE_ORDER__
+        #endif
+        };
+
         inline bool is_valid_base64_char(char c)
         {
             return ((c >= 'A') && (c <= 'Z'))
@@ -23,48 +36,82 @@ namespace persistence
             'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
         };
 
-        using triplet_type = std::uint32_t;
-
         /**
-        * Produces the Base64-encoding of a sequence of bytes.
-        *
-        * @param concat_bytes An integer storing data on the 3 least-significant bytes.
-        * @param output A string to receive 4 Base64-encoded characters.
-        */
-        inline void encode_triplet(triplet_type concat_bytes, std::string& output)
+         * Produces the Base64-encoding of a sequence of bytes.
+         *
+         * @param value An integer storing data on the 3 least-significant bytes.
+         * @param out A pointer to a string to receive 4 Base64-encoded characters.
+         */
+        inline void encode_triplet(std::uint32_t value, char* out)
         {
-            output.push_back(encode_table[(concat_bytes >> 18) & 0x3F]);
-            output.push_back(encode_table[(concat_bytes >> 12) & 0x3F]);
-            output.push_back(encode_table[(concat_bytes >>  6) & 0x3F]);
-            output.push_back(encode_table[concat_bytes & 0x3F]);
+            *out++ = encode_table[(value >> 18) & 0x3F];
+            *out++ = encode_table[(value >> 12) & 0x3F];
+            *out++ = encode_table[(value >>  6) & 0x3F];
+            *out++ = encode_table[value & 0x3F];
         }
 
         /**
-        * Produces the Base64-encoding of a sequence of bytes.
-        *
-        * @param a First byte to encode.
-        * @param b Second byte to encode.
-        * @param c Third byte to encode.
-        * @param output A string to receive 4 Base64-encoded characters.
-        */
-        inline void encode_triplet(std::byte a, std::byte b, std::byte c, std::string& output)
+         * Produces the Base64-encoding of the first 6 bytes of an integer.
+         *
+         * @param value An integer with big-endian representation.
+         * @param out A pointer to a string to receive 8 Base64-encoded characters.
+         */
+        inline void encode_sextet_big_endian(std::uint64_t value, char* out)
         {
-            triplet_type concat_bytes =
-                (std::to_integer<triplet_type>(a) << 16) |
-                (std::to_integer<triplet_type>(b) <<  8) |
-                std::to_integer<triplet_type>(c)
+            *out++ = encode_table[(value >> 58) & 0x3F];
+            *out++ = encode_table[(value >> 52) & 0x3F];
+            *out++ = encode_table[(value >> 46) & 0x3F];
+            *out++ = encode_table[(value >> 40) & 0x3F];
+            *out++ = encode_table[(value >> 34) & 0x3F];
+            *out++ = encode_table[(value >> 28) & 0x3F];
+            *out++ = encode_table[(value >> 22) & 0x3F];
+            *out++ = encode_table[(value >> 16) & 0x3F];
+        }
+
+        /**
+         * Produces the Base64-encoding of the first 6 bytes of an integer.
+         *
+         * @param value An integer with little-endian representation.
+         * @param out A pointer to a string to receive 8 Base64-encoded characters.
+         */
+        inline void encode_sextet_little_endian(std::uint64_t value, char* out)
+        {
+            *out++ = encode_table[(value >> 2) & 0b00111111];
+            *out++ = encode_table[((value & 0b00000011) << 4) | ((value >> 12) & 0b00001111)];
+            *out++ = encode_table[(((value >> 8) & 0b00001111) << 2) | ((value >> 22) & 0b00000011)];
+            *out++ = encode_table[(value >> 16) & 0b00111111];
+            *out++ = encode_table[(value >> 26) & 0b00111111];
+            *out++ = encode_table[(((value >> 24) & 0b00000011) << 4) | ((value >> 36) & 0b00001111)];
+            *out++ = encode_table[(((value >> 32) & 0b00001111) << 2) | ((value >> 46) & 0b00000011)];
+            *out++ = encode_table[(value >> 40) & 0b00111111];
+        }
+
+        /**
+         * Produces the Base64-encoding of a sequence of bytes.
+         *
+         * @param a First byte to encode.
+         * @param b Second byte to encode.
+         * @param c Third byte to encode.
+         * @param out A pointer to a string to receive 4 Base64-encoded characters.
+         */
+        inline void encode_triplet(std::byte a, std::byte b, std::byte c, char* out)
+        {
+            std::uint32_t concat_bytes =
+                (std::to_integer<std::uint32_t>(a) << 16) |
+                (std::to_integer<std::uint32_t>(b) <<  8) |
+                std::to_integer<std::uint32_t>(c)
             ;
-            encode_triplet(concat_bytes, output);
+            encode_triplet(concat_bytes, out);
         }
     }
 
     /**
-    * Generates the Base64-encoded representation of a raw sequence of bytes.
-    *
-    * @param input A sequence of bytes.
-    * @param output A Base64-encoded string.
-    * @return True on success.
-    */
+     * Generates the Base64-encoded representation of a raw sequence of bytes.
+     *
+     * @param input A sequence of bytes.
+     * @param output A Base64-encoded string.
+     * @return True on success.
+     */
     inline bool base64_encode(const byte_view& input, std::string& output)
     {
         using detail::encode_triplet;
@@ -74,47 +121,62 @@ namespace persistence
             return true;
         }
 
+        std::size_t fast_sextets = input.size() / 24 * 4;  // round to multiples of 8 bytes to ensure pointer stays in addressable range
         std::size_t full_triplets = input.size() / 3;
+        std::size_t remaining_triplets = full_triplets - fast_sextets * 2;
         std::size_t remaining_bytes = input.size() % 3;
         std::size_t output_size = full_triplets * 4 + (remaining_bytes > 0 ? 4 : 0);
-        output.reserve(output_size);
+        output.resize(output_size);
 
         const std::byte* p = input.data();
+        char* r = output.data();
 
-        // process all complete triplets
-        for (std::size_t i = 0; i < full_triplets; ++i) {
+        // iterate over data with 64-bit words, processing full sextets
+        for (std::size_t i = 0; i < fast_sextets; ++i) {
+            uint64_t value = *reinterpret_cast<const uint64_t*>(p);
+            if constexpr (detail::endianness::native == detail::endianness::little) {
+                detail::encode_sextet_little_endian(value, r);
+            } else if constexpr (detail::endianness::native == detail::endianness::big) {
+                detail::encode_sextet_big_endian(value, r);
+            }
+            p += 6;
+            r += 8;
+        }
+
+        // process remaining full triplets (if any)
+        for (std::size_t i = 0; i < remaining_triplets; ++i) {
             std::byte a = *p++;
             std::byte b = *p++;
             std::byte c = *p++;
-            encode_triplet(a, b, c, output);
+            encode_triplet(a, b, c, r);
+            r += 4;
         }
 
         // process remaining bytes (last incomplete triplet)
         if (remaining_bytes == 2) {
             std::byte a = input[input.size() - 2];
             std::byte b = input[input.size() - 1];
-            encode_triplet(a, b, std::byte{0x00}, output);
-            output.pop_back();
-            output.push_back('=');
+            encode_triplet(a, b, std::byte{0x00}, r);
+            r += 3;
+            *r++ = '=';
         } else if (remaining_bytes == 1) {
             std::byte a = input.back();
-            encode_triplet(a, std::byte{0x00}, std::byte{0x00}, output);
-            output.pop_back();
-            output.pop_back();
-            output.push_back('=');
-            output.push_back('=');
+            encode_triplet(a, std::byte{0x00}, std::byte{0x00}, r);
+            r += 2;
+            *r++ = '=';
+            *r++ = '=';
         }
 
         return true;
     }
 
     /**
-    * Generates the Base64-encoded representation of a raw sequence of bytes.
-    *
-    * @param input A sequence of bytes.
-    * @param output A Base64-encoded string.
-    * @return True on success.
-    */
+     * Generates the Base64-encoded representation of a raw sequence of bytes.
+     *
+     * @param input A sequence of bytes.
+     * @param output A Base64-encoded string.
+     * @return True on success.
+     */
     inline bool base64_encode(const byte_vector& input, std::string& output)
     {
         return base64_encode(
@@ -127,12 +189,12 @@ namespace persistence
     }
 
     /**
-    * Generates the Base64-encoded representation of a string.
-    *
-    * @param input An input string.
-    * @param output A Base64-encoded string.
-    * @return True on success.
-    */
+     * Generates the Base64-encoded representation of a string.
+     *
+     * @param input An input string.
+     * @param output A Base64-encoded string.
+     * @return True on success.
+     */
     inline bool base64_encode(const std::string_view& input, std::string& output)
     {
         return base64_encode(
@@ -165,54 +227,40 @@ namespace persistence
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
         };
 
-        using quadruplet_type = std::uint32_t;
-
         /**
-        * Produces the Base64-decoding of a sequence of bytes.
-        *
-        * @param concat_bytes An integer storing data on the 4 least-significant bytes.
-        * @param output A vector to receive 3 Base64-decoded bytes.
-        */
-        inline void decode_quadruplet(quadruplet_type concat_bytes, byte_vector& out)
+         * Produces the Base64-decoding of a sequence of characters.
+         *
+         * @param in_a First byte to decode.
+         * @param in_b Second byte to decode.
+         * @param in_c Third byte to decode.
+         * @param in_d Fourth byte to decode.
+         * @param out A pointer to a buffer to receive 3 Base64-decoded bytes.
+         */
+        inline bool decode_quadruplet(char in_a, char in_b, char in_c, char in_d, std::byte* out)
         {
-            out.push_back(std::byte((concat_bytes >> 16) & 0xFF));
-            out.push_back(std::byte((concat_bytes >> 8) & 0xFF));
-            out.push_back(std::byte(concat_bytes & 0xFF));
-        }
+            auto out_a = decode_table[in_a];
+            auto out_b = decode_table[in_b];
+            auto out_c = decode_table[in_c];
+            auto out_d = decode_table[in_d];
 
-        /**
-        * Produces the Base64-decoding of a sequence of characters.
-        *
-        * @param in_a First byte to decode.
-        * @param in_b Second byte to decode.
-        * @param in_c Third byte to decode.
-        * @param in_d Fourth byte to decode.
-        * @param output A vector to receive 3 Base64-decoded bytes.
-        */
-        inline bool decode_quadruplet(char in_a, char in_b, char in_c, char in_d, byte_vector& out)
-        {
-            auto out_a = static_cast<quadruplet_type>(decode_table[in_a]);
-            auto out_b = static_cast<quadruplet_type>(decode_table[in_b]);
-            auto out_c = static_cast<quadruplet_type>(decode_table[in_c]);
-            auto out_d = static_cast<quadruplet_type>(decode_table[in_d]);
-
-            PERSISTENCE_IF_UNLIKELY(((out_a | out_b | out_c | out_d) & ~static_cast<quadruplet_type>(0x3F)) != 0) {
+            PERSISTENCE_IF_UNLIKELY(((out_a | out_b | out_c | out_d) & 0b11000000) != 0) {
                 return false;
             }
 
-            quadruplet_type concat_bytes = (out_a << 18) | (out_b << 12) | (out_c << 6) | out_d;
-            decode_quadruplet(concat_bytes, out);
+            *out++ = std::byte((out_a << 2) | (out_b >> 4));
+            *out++ = std::byte(((out_b & 0b00001111) << 4) | (out_c >> 2));
+            *out++ = std::byte(((out_c & 0b00000011) << 6) | out_d);
             return true;
         }
     }
 
     /**
-    * Parses the Base64-encoded representation of a sequence of bytes.
-    *
-    * @param input A Base64-encoded string.
-    * @param output A sequence of bytes.
-    * @return True if parsing is successful, false if input is invalid.
-    */
+     * Parses the Base64-encoded representation of a sequence of bytes.
+     *
+     * @param input A Base64-encoded string.
+     * @param output A sequence of bytes.
+     * @return True if parsing is successful, false if input is invalid.
+     */
     inline bool base64_decode(const std::string_view& input, byte_vector& output)
     {
         using detail::decode_quadruplet;
@@ -227,9 +275,11 @@ namespace persistence
         }
 
         std::size_t quadruplets = input.size() / 4;
-        output.reserve(quadruplets * 3);
+        output.resize(quadruplets * 3);
 
         const char* p = input.data();
+        std::byte* r = output.data();
+
         for (std::size_t i = 1; i < quadruplets; ++i) {
             char a = *p++;
             char b = *p++;
@@ -237,20 +287,26 @@ namespace persistence
             char d = *p++;
 
             // process all but last quadruplet
-            bool result = decode_quadruplet(a, b, c, d, output);
+            bool result = decode_quadruplet(a, b, c, d, r);
             if (!result) {
                 return false;
             }
+
+            r += 3;
         }
 
         // process last quadruplet
         if (input[input.size() - 2] == '=') {
+            if (input[input.size() - 1] != '=') {
+                return false;
+            }
+
             bool result = decode_quadruplet(
                 input[input.size() - 4],
                 input[input.size() - 3],
                 'A',
                 'A',
-                output
+                r
             );
             if (!result) {
                 return false;
@@ -263,7 +319,7 @@ namespace persistence
                 input[input.size() - 3],
                 input[input.size() - 2],
                 'A',
-                output
+                r
             );
             if (!result) {
                 return false;
@@ -275,7 +331,7 @@ namespace persistence
                 input[input.size() - 3],
                 input[input.size() - 2],
                 input[input.size() - 1],
-                output
+                r
             );
             if (!result) {
                 return false;
